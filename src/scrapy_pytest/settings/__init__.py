583a39eef1a1e8dd628748b346d9a39b4bb9c8c5
barnettxxf <15102096586@163.com>
2019-06-15 11:14:57 +0800

    modify empty fixture

--- src/scrapy_pytest/settings/__init__.py
+++ src/scrapy_pytest/settings/__init__.py
@@ -6,7 +6,7 @@ from . import default_settings as default_settings_module
 
 class Settings(BaseSettings):
     # against run ~.__init__ twice
-    flag = True
+    _flag = True
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(Settings, '_instance'):
@@ -14,9 +14,9 @@ class Settings(BaseSettings):
         return Settings._instance
 
     def __init__(self, values=None, priority='project'):
-        if not Settings.flag:
+        if not Settings._flag:
             return
-        Settings.flag = False
+        Settings._flag = False
         super().__init__()
         self.setmodule(default_settings_module, 'default')
         for name, val in six.iteritems(self):
