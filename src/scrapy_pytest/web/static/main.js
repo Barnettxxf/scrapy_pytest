691d5aec32db4375dcaa34dac61ffe2da733d0ca
barnettxxf <15102096586@163.com>
2019-06-22 23:22:30 +0800

    add main.js

--- src/scrapy_pytest/web/static/main.js
+++ src/scrapy_pytest/web/static/main.js
@@ -1,16 +1,16 @@
 (function () {
-    $('#commit').on('click', function (e) {
-        e.preventDefault();
+    $('select').on('change', function (e) {
+        // e.preventDefault();
 
-        let storage = $('#storage').val();
-        let spider = $('#spider').val();
+        let storage = $('#storage').val().trim();
+        let spider = $('#spider').val().trim();
 
         superagent.get(`/filter?storage=${storage}&spider=${spider}`).end(function (err, data) {
             if (err) throw err;
 
             let str = '';
             for (let row of data.body.rows) {
-                str = str + `<td><input type="checkbox"></td><tr><td>${row.id}</td><td>${row.storage}</td><td>${row.spider}</td><td>${row.parse_func}</td><td>${row.url}</td><td>${JSON.stringify(row.meta)}</td><td><a href="/del?request_id=${row.id}">delete</a></td></td></tr>`;
+                str = str + `<tr><td><input type="checkbox"></td><td>${row.id}</td><td>${row.storage}</td><td>${row.spider}</td><td>${row.parse_func}</td><td>${row.url}</td><td>${JSON.stringify(row.meta)}</td><td><a href="/del?request_id=${row.id}">delete</a></td></td></tr>`;
             }
             console.log(str);
             $('#content').html(str);
