ce8cde2a480b637f5bca8c0c12e5a4b25bdfb90d
barnettxxf <15102096586@163.com>
2019-06-22 14:21:01 +0800

    add plugins ymspider

--- tests/auto_gen_tests/baidu/conftest.py
+++ tests/auto_gen_tests/baidu/conftest.py
@@ -16,9 +16,6 @@ def empty(request):
     request.addfinalizer(rsp_factory.close)
 
 
-pytest_plugins = ('scrapy_pytest.plugins.ymspider','scrapy_pytest.plugins.ymspider')
-
-
 @pytest.fixture
 def BaiduSpider():
     return _BaiduSpider
