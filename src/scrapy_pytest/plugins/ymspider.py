d21289bf1c811fcccea4da5c1763a13a9e3c84e4
barnettxxf <15102096586@163.com>
2019-06-22 22:38:33 +0800

    add plugins ymspider

--- src/scrapy_pytest/plugins/ymspider.py
+++ src/scrapy_pytest/plugins/ymspider.py
@@ -10,7 +10,15 @@ def scheduler(monkeypatch):
     def fake_publish(*args, **kwargs):
         pass
 
+    def fake_ack(*args, **kwargs):
+        pass
+
+    def fake_nack(*args, **kwargs):
+        pass
+
     monkeypatch.setattr('scheduler.Scheduler.publish', fake_publish)
+    monkeypatch.setattr('scheduler.Scheduler.ack', fake_ack)
+    monkeypatch.setattr('scheduler.Scheduler.fake_nack', fake_nack)
 
 
 @pytest.fixture(autouse=True)
@@ -23,7 +31,7 @@ def monitor(monkeypatch):
             return FakeMonitor()
 
         def counter(self, *args, **kwargs):
-            return FakeMonitor
+            return FakeMonitor()
 
     monkeypatch.setattr('scrapy.Spider.monitor', FakeMonitor(), raising=False)
     monkeypatch.setattr('cyborg.downloaders.scrapy.IdleSpider.monitor', FakeMonitor(), raising=False)
